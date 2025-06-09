eab6e7f4d18ef735d9a532a5a93f7d2590486419
sogbladi <zaouibrahim5593@gmail.com>
2025-06-09 02:03:40 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,26 @@
-// sw.js
-self.addEventListener('install', event => {
-  console.log('Service Worker installed');
+const CACHE_NAME = "ibrazaoui-cache-v1";
+const urlsToCache = [
+  "/my-usedstore/",
+  "/my-usedstore/index.html",
+  "/my-usedstore/manifest.json",
+  "/my-usedstore/sw.js",
+  "/my-usedstore/icon-192.png",
+  "/my-usedstore/icon-512.png"
+  // أضف هنا المزيد من الملفات (CSS, JS, صور) إذا كانت موجودة
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return new Response('You are offline');
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
